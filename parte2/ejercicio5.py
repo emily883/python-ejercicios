d893432084ab153e4495e01cb8738f389357c36e
Emily <emily.moraima.cruz@hotmail.com>
2020-06-16 13:31:36 -0400

    decimal a binario

---
+++ parte2/ejercicio5.py
@@ -0,0 +1,6 @@
+print("Hare algo bien sencillo para ti :v ")
+numero=int(input("Dime un numero y te lo devuelvo en binario :v: "))
+
+binario = bin(numero)
+
+print(f"Tu numero en binario es {binario} :v ")
