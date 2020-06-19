a4064097d5073eb9fdf0a2b169afa8d97040c9ab
Emily <emily.moraima.cruz@hotmail.com>
2020-06-19 11:34:13 -0400

    palabra comienza con tal letra en una lista

---
+++ parte2/ejercicio8.py
@@ -0,0 +1,17 @@
+
+
+
+nombres= ["emily", "julianys", "yaritza"]
+letra=input("indique una letra: ")
+conteo = 0
+
+for i in nombres:
+    if letra == i[0]:
+        conteo+=1
+
+print(f"Tu lista tiene {conteo}, q comienzan con {letra}")
+
+
+
+
+
